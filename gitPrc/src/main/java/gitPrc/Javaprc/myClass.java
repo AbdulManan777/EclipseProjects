eab3bd4b2e27346b18c79615650d7c18c90c4499
Abdul Manan <74492404+AbdulManan777@users.noreply.github.com>
2021-09-18 15:49:59 +0500

    Maven Project on GitHub Updated

--- gitPrc/src/main/java/gitPrc/Javaprc/myClass.java
+++ gitPrc/src/main/java/gitPrc/Javaprc/myClass.java
@@ -5,6 +5,7 @@ public class myClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("I am new to Java");
+		System.out.println("I have sucessfully integrate eclipse and gitHub");
 
 	}
 

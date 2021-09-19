ac456f35eb54c40696129c67f5e7d4e40b4d947d
Abdul Manan <i190500@nu.edu.pk>
2021-09-19 12:33:30 +0500

    Committing to gitHubMaven Project

---
+++ eclipseCode/src/main/java/github/eclipseCode/PracticeClass.java
@@ -0,0 +1,11 @@
+package github.eclipseCode;
+
+public class PracticeClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Working on Java");
+
+	}
+
+}

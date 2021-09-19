d1c1edd75ff86274521dffd73d951ee48b3d3c83
Abdul Manan <i190500@nu.edu.pk>
2021-09-19 12:54:51 +0500

    Coding Commit

--- eclipseCode/src/main/java/github/eclipseCode/PracticeClass.java
+++ eclipseCode/src/main/java/github/eclipseCode/PracticeClass.java
@@ -5,6 +5,7 @@ public class PracticeClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Working on Java");
+		System.out.println("New to Java");
 
 	}
 
